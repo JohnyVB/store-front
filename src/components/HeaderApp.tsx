de1b51e4e1fa6a6c8b87936253501491d760b3ab
Johny Villegas <55726826+JohnyVB@users.noreply.github.com>
2024-03-08 16:59:08 -0500

    Primer commit

---
+++ src/components/HeaderApp.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const HeaderApp = () => {
+  return (
+    <div className='h-[70px] bg-blue-500 text-white flex items-center justify-center space-x-4'>
+        <h1>TechStore</h1>
+      <input
+        type="text"
+        placeholder="Search..."
+        className="text-black w-[500px] px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+      <h1>Mis compras</h1>
+      <h1>Favoritos</h1>
+      <h1>Carrito</h1>
+      <h1>User</h1>
+    </div>
+  )
+}
